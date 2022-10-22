ded9ff9867750d9fcb6bc592a8db47629f99670c
Karamvir-Bains <kbains2000@gmail.com>
2022-10-22 10:36:40 -0600

    Added iss_promised.js and index2.js

---
+++ index2.js
@@ -0,0 +1,15 @@
+const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    let day = new Date(pass.risetime);
+    day = day.toUTCString();
+    const duration = pass.duration;
+    console.log(`Next pass at ${day} for ${duration} seconds!`);
+  }
+};
+
+
+nextISSTimesForMyLocation()
+  .then(passTimes => printPassTimes(passTimes))
+  .catch(error => console.log("It didn't work: ", error.message));
